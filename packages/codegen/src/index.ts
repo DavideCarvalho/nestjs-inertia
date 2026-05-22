6efdee92fc1b99e9d3ac3dd10a97133aed7317d8
Davi de Carvalho <davi.carvalho96@gmail.com>
2026-05-22 13:49:22 -0300

    chore(release): bump all packages to 0.5.0-alpha.0

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.4.0-alpha.0';
+export const VERSION = '0.5.0-alpha.0';
 
 export { defineConfig } from './config/define-config.js';
 export { loadConfig } from './config/load-config.js';
